4527708088758ef84905d6684b9f3409e3d06f95
Celine Bougel <celine.bougel5@laposte.net>
2023-09-29 14:11:31 +0200

    Minor changes after test koplsReDummy on simulated example.

--- codes/R_software/functions/Step0_2_2_koplsReDummy.R
+++ codes/R_software/functions/Step0_2_2_koplsReDummy.R
@@ -7,11 +7,23 @@
 #' `classVect`: The reconstructed integer class vector.
 #'
 #' @examples
-#' matrix_test <- c(5, 1, 2, 3, 4, 3, 2, 4, 3, 1, 3)
-#' Y <- koplsDummy(class = matrix_test, numClasses = NA)
-#' Y$matrix
+#' class_test <- c(5, 1, 2, 3, 4, 3, 2, 4, 3, 1, 3)
+#' #numClasses needs to be NA for this example
+#' Y <- koplsDummy(class = class_test, numClasses = NA)
+#' X <- koplsReDummy(Y$matrix)
 
 koplsReDummy <- function(Y){
+  # Variable format control
+  if(is.null(Y)){stop("Y must be contain matrix.")
+  }else{
+    test <- Y %in% c(0,1)
+    for(i in 1:base::length(test)){
+      if(test[i] == FALSE){
+        stop("Y must contains only 0 and 1 values.")
+      }
+    }
+  }
+  
   # Create an empty vector
   classVect <- base::rep(x = NA, times = nrow(Y))
   
