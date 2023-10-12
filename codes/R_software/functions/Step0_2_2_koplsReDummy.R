3f752fe896ed3fc750a56d73c8ae0ed95376ee35
Celine Bougel <celine.bougel5@laposte.net>
2023-10-12 15:17:22 +0200

    Check koplsReDummy with already published code, Add package author(s) references, Consider function validated and tested by authors.

--- codes/R_software/functions/Step0_2_2_koplsReDummy.R
+++ codes/R_software/functions/Step0_2_2_koplsReDummy.R
@@ -1,26 +1,36 @@
 #' koplsReDummy
 #' Reconstructs a (integer) class vector from a binary (dummy) matrix.
+#' 
+#' # ------------------------------------------------------------------------ #
+#' This file is part of the K-OPLS package, developed by Max Bylesjo, 
+#' University of Umea, Judy Fonville and Mattias Rantalainen, Imperial College.
+#' 
+#' Copyright (c) 2007-2010 Max Bylesjo, Judy Fonville and Mattias Rantalainen 
+#' 
+#' This code has been extended and adapted under the terms of the GNU General 
+#' Public License version 2 as published by the Free Software Foundation.
+#' # ------------------------------------------------------------------------ #
 #'
-#' @param Y: a dummy matrix.
+#' @param Y: matrix. A dummy matrix to be transformed into a numeric vector.
 #'
 #' @return 
-#' `classVect`: The reconstructed integer class vector.
+#' `classVect`: matrix. The reconstructed integer class vector.
 #'
 #' @examples
-#' class_test <- c(5, 1, 2, 3, 4, 3, 2, 4, 3, 1, 3)
+#' class_test <- base::matrix(c(5, 1, 2, 3, 4, 3, 2, 4, 3, 1, 3),
+#'                            ncol = 1)
 #' #numClasses needs to be NA for this example
 #' Y <- koplsDummy(class = class_test, numClasses = NA)
 #' X <- koplsReDummy(Y$matrix)
 
 koplsReDummy <- function(Y){
   # Variable format control
-  if(is.null(Y)){stop("Y must be contain matrix.")
+  if(is.null(Y)){stop("Y must be contain  a dummy matrix.")
   }else{
-    test <- Y %in% c(0,1)
-    for(i in 1:base::length(test)){
-      if(test[i] == FALSE){
-        stop("Y must contains only 0 and 1 values.")
-      }
+    if(!is.matrix(Y)){stop("Y must be a matrix.")}
+    test <- Y %in% c(0, 1)
+    if(all(test) != TRUE){
+      stop("Y must contains only 0 and 1 values.")
     }
   }
   
@@ -32,6 +42,6 @@ koplsReDummy <- function(Y){
     classVect[Y[, i] == 1] <- i
   }
   
-  # Return a (integer) class vector from a binary matrix
+  # Return the reverted dummy matrix to the original vector of class labels
   return(classVect)
 }
