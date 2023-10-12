723db440add21d5372f187b3ce0eb5300d4b3da3
Celine Bougel <celine.bougel5@laposte.net>
2023-10-12 15:23:31 +0200

    Check koplsCenterKTrTr with already published code, Add package author(s) references, Consider function validated and tested by authors.

--- codes/R_software/functions/Step0_2_7_koplsCenterKTrTr.R
+++ codes/R_software/functions/Step0_2_7_koplsCenterKTrTr.R
@@ -1,17 +1,27 @@
 #' koplsCenterKTrTr
 #' Centering function for the training kernel, which is constructed
 #' from the training matrix Xtr as K = <phi(Xtr), phi(Xtr)>.
+#' 
+#' # ------------------------------------------------------------------------ #
+#' This file is part of the K-OPLS package, developed by Max Bylesjo, 
+#' University of Umea, Judy Fonville and Mattias Rantalainen, Imperial College.
+#' 
+#' Copyright (c) 2007-2010 Max Bylesjo, Judy Fonville and Mattias Rantalainen 
+#' 
+#' This code has been extended and adapted under the terms of the GNU General 
+#' Public License version 2 as published by the Free Software Foundation.
+#' # ------------------------------------------------------------------------ #
 #'
-#' @param K: Training kernel matrix; K = <phi(Xtr), phi(Xtr)>. 
+#' @param K: matrix. Contains the training kernel matrix; K = <phi(Xtr), phi(Xtr)>. 
 #'
 #' @return
-#' `K`: The centered kernel matrix.
+#' `K`: matrix. The centered kernel matrix.
 #'
 #' @examples
-#' K <- matrix(1:25, nrow = 5, ncol = 5)
+#' K <- base::matrix(1:25, nrow = 5, ncol = 5)
 #' test <- koplsCenterKTrTr(K = K)
 #' test
-#' 
+
 koplsCenterKTrTr <- function(K){
   # Variable format control
   if(!is.matrix(K)){stop("K is not a matrix.")}
@@ -24,7 +34,7 @@ koplsCenterKTrTr <- function(K){
   scaling_matrix <- (1/nrow(K)) * (I_n %*% t(I_n))
   
   # Update K
-  K = (I- scaling_matrix) %*% K %*% (I - scaling_matrix);
+  K = (I- scaling_matrix) %*% K %*% (I - scaling_matrix)
   
   # Return the centered kernel matrix
   return(K)
