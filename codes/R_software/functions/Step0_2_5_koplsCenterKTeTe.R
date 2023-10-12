83f08530c72905a8d829af03cc27d6955fb72bce
Celine Bougel <celine.bougel5@laposte.net>
2023-10-12 15:20:46 +0200

    Check koplsCenterKTeTe with already published code, Add package author(s) references, Consider function validated and tested by authors.

--- codes/R_software/functions/Step0_2_5_koplsCenterKTeTe.R
+++ codes/R_software/functions/Step0_2_5_koplsCenterKTeTe.R
@@ -3,16 +3,29 @@
 #' from the test matrix Xte as KteTe = <phi(Xte), phi(Xte)>.
 #' Requires additional (un-centered) kernels KteTr and KteTr to
 #' estimate mean values.
+#' 
+#' # ------------------------------------------------------------------------ #
+#' This file is part of the K-OPLS package, developed by Max Bylesjo, 
+#' University of Umea, Judy Fonville and Mattias Rantalainen, Imperial College.
+#' 
+#' Copyright (c) 2007-2010 Max Bylesjo, Judy Fonville and Mattias Rantalainen 
+#' 
+#' This code has been extended and adapted under the terms of the GNU General 
+#' Public License version 2 as published by the Free Software Foundation.
+#' # ------------------------------------------------------------------------ #
 #'
-#' @param KteTe: Test kernel matrix, KteTe = <phi(Xte), phi(Xte)>.  
-#' @param KteTr: Test/training kernel matrix, KteTr = <phi(Xte), phi(Xtr)>.
-#' @param KtrTr: Training kernel matrix, KtrTr = <phi(Xtr), phi(Xtr)>.
+#' @param KteTe: matrix. Contains the test kernel matrix, 
+#' KteTe = <phi(Xte), phi(Xte)>.  
+#' @param KteTr: matrix. Contains the hybrid test/training kernel matrix, 
+#' KteTr = <phi(Xte), phi(Xtr)>.
+#' @param KtrTr: matrix. Contains the training kernel matrix, 
+#' KtrTr = <phi(Xtr), phi(Xtr)>.
 #'
 #' @return
-#' `KteTe` the centered test kernel matrix
+#' `KteTe`: matrix. The centered test kernel matrix.
 #'
 #' @examples
-#' KteTr <- matrix(1:25, nrow = 5, ncol = 5)
+#' KteTr <- base::matrix(1:25, nrow = 5, ncol = 5)
 #' KteTe <- matrix(1:25, nrow = 5, ncol = 5)
 #' KtrTr <- matrix(1:25, nrow = 5, ncol = 5)
 #' test <- koplsCenterKTeTe(KteTe = KteTe, KteTr = KteTr, KtrTr = KtrTr)
@@ -28,13 +41,13 @@ koplsCenterKTeTe <- function(KteTe, KteTr, KtrTr){
   I_nTrain <- base::rep(x = 1, times = ncol(KteTr))
   nTrain <- ncol(KteTr)
   
-  I <- base::diag(ncol(KteTe))
-  I_n <- base::rep(x = 1, times = ncol(KteTe))
-  n <- ncol(KteTe)
+  I <- base::diag(nrow(KteTr))
+  I_n <- base::rep(x = 1, times = nrow(KteTr))
+  n <- nrow(KteTr)
   
   # Center the kernel
-  D_te <- (1/nTrain) %*% I_n %*% t(I_nTrain)
-  KteTe <- KteTe - D_te%*%t(KteTr) - KteTr%*%t(D_te) + D_te%*%KtrTr %*%t(D_te)
+  D_te <- (1/nTrain) * I_n %*% t(I_nTrain)
+  KteTe <- KteTe - D_te%*%t(KteTr) - KteTr%*%t(D_te) + D_te%*%KtrTr%*%t(D_te)
   
   # Return the centered test kernel matrix
   return(KteTe)
