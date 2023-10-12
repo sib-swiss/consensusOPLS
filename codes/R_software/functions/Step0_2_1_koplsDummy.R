0aedbeb9269e4114c8c5e1e295813ecf1e0d464c
Celine Bougel <celine.bougel5@laposte.net>
2023-10-12 15:15:42 +0200

    Check koplsDummy with already^published code, Add package author(s) references, Consider function validated and tested by authors.

--- codes/R_software/functions/Step0_2_1_koplsDummy.R
+++ codes/R_software/functions/Step0_2_1_koplsDummy.R
@@ -1,49 +1,73 @@
 #' KoplsDummy
 #' Transform a vector into a dummy (binary) matrix.
+#' 
+#' # ------------------------------------------------------------------------ #
+#' This file is part of the K-OPLS package, developed by Max Bylesjo, 
+#' University of Umea, Judy Fonville and Mattias Rantalainen, Imperial College.
+#' 
+#' Copyright (c) 2007-2010 Max Bylesjo, Judy Fonville and Mattias Rantalainen 
+#' 
+#' This code has been extended and adapted under the terms of the GNU General 
+#' Public License version 2 as published by the Free Software Foundation.
+#' # ------------------------------------------------------------------------ #
 #'
-#' @param class: integer vector with classes to define.
-#' @param numClasses: pre-defined the number of classes in the output. By 
-#' default is equal to the length of `class` vector.
+#' @param class: matrix with 1 column. Integer vector with classes to define.
+#' @param numClasses: numeric. Pre-defined the number of classes in the output. 
+#' By default (if numClasses is missing), is equal to the length of `class` 
+#' vector. Otherwise, the class vector is split into `numclasses` pieces.
 #'
 #' @return
-#' `matrix`: A matrix with rows corresponding to observations and columns 
-#' to classes. Each element in matrix is either one (observation belongs to 
-#' class) or zero (observation does not belong to class).
-#' `labels_sorted`: The class labels that are found in class in sorted order.
+#' `matrix`: matrix. Results of the dummy matrix, with rows corresponding to 
+#' observations and columns to classes. Each element in matrix is either one 
+#' (observation belongs to class) or zero (observation does not belong to class).
+#' `labels_sorted`: numerical vector. The class labels that are found in class 
+#' in sorted order.
 #'
 #' @examples
-#' class <- c(5, 1, 2, 3, 4, 3, 2, 4, 3, 1, 3)
+#' class <- base::matrix(c(5, 1, 2, 3, 4, 3, 2, 4, 3, 1, 3),
+#'                       ncol = 1)
 #' numClasses <- 2
 #' Y <- koplsDummy(class = class, numClasses = numClasses)
 #' Y$matrix
 #' Y$labels_sorted
 
 koplsDummy <- function(class, numClasses = NA){
-  # Define parameters
-  labels <- base::unique(class)
-  labels_sorted <- base::sort(labels, decreasing = FALSE)
-  samples_number <- base::length(class)
-  
   # Variable format control
-  if(is.null(class)){stop("class must be contain an integer vector.")}
+  if(is.null(class)){stop("class must be contain an integer vector.")
+  } else{
+    if(!is.matrix(class)){
+      stop("class must be a matrix.")
+    } else{
+      if(ncol(class) != 1){
+        stop("class must be a matrix with only 1 column.")
+      }
+    }
+  }
   if(is.na(numClasses)){
-    sample_labels <- base::length(labels)
+    sample_labels <- base::length(base::unique(class))
     ncol <- sample_labels
     default <- TRUE
   } else{
+    if(!is.numeric(numClasses)){stop("numClasses must be numeric.")}
     sample_labels <- base::cut(x = class, breaks = numClasses,
                                ordered_result = TRUE)
     ncol <- base::length(levels(sample_labels))
     default <- FALSE
   }
   
+  # Define parameters
+  labels <- base::sort(base::unique(class), decreasing = FALSE)
+  
   # Matrix initialization
-  dummy <- base::matrix(data = 0, nrow = samples_number, ncol = ncol)
+  dummy <- base::matrix(data = 0, nrow = base::length(class), ncol = ncol)
   
   # Search for class membership
   for(i in 1:ncol){
     if(default){
-      dummy[class %in% labels_sorted[i], i] <- 1
+      dummy[class == labels[i], i] <- 1
+      
+      # And change dummy's column names
+      colnames(dummy) <- labels
     } else{
       labels_cut <- cbind("lower" = as.numeric(base::sub("\\((.+),.*", "\\1", 
                                                          levels(sample_labels)[i],
@@ -53,9 +77,16 @@ koplsDummy <- function(class, numClasses = NA){
                                                          fixed = FALSE)))
       dummy[(class > labels_cut[1, "lower"] & 
                class <= labels_cut[1, "upper"]), i] <- 1
+      
+      # And change dummy's column names
+      colnames(dummy) <- c(labels_cut[1, "lower"], labels_cut[1, "upper"])
     }
   }
+  
+  # Change dummy row names
+  rownames(dummy) <- rownames(class)
+  
   # Return a list with 2 elements
   return(list("matrix" = dummy,
-              "labels_sorted" = labels_sorted))
+              "labels_sorted" = labels))
 }
