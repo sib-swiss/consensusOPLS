c23488f05d043f43337cc7eab187a6ca73bdcdfb
Celine Bougel <celine.bougel5@laposte.net>
2023-10-23 08:51:42 +0200

    Check koplsBasicClassify with already publish code, Add package author(s) references, Consider function validated and tested by authors.

--- codes/R_software/functions/Step0_2_12_koplsBasicClassify.R
+++ codes/R_software/functions/Step0_2_12_koplsBasicClassify.R
@@ -1,6 +1,16 @@
 #' koplsBasicClassify
 #' Classification function that assesses class belonging of a predicted
 #' response in 'data' based on a fixed threshold 'k'.
+#' 
+#' # ------------------------------------------------------------------------ #
+#' This file is part of the K-OPLS package, developed by Max Bylesjo, 
+#' University of Umea, Judy Fonville and Mattias Rantalainen, Imperial College.
+#' 
+#' Copyright (c) 2007-2010 Max Bylesjo, Judy Fonville and Mattias Rantalainen 
+#' 
+#' This code has been extended and adapted under the terms of the GNU General 
+#' Public License version 2 as published by the Free Software Foundation.
+#' # ------------------------------------------------------------------------ #
 #'
 #' @param data: matrix containing the predicted response matrix Y,
 #' where columns denote classes and rows observations.
@@ -10,13 +20,13 @@
 #' `predClass`: the predicted class(es) of `data` given `k`.
 #'
 #' @examples
-#'data <- as.matrix(data.frame(x1 = c(2, 1, 5, 1),
-#'                             x2 = c(7, 1, 1, 5),
-#'                             x3 = c(9, 5, 4, 9),
-#'                             x4 = c(3, 4, 1, 2)))
-#'k <- 4
-#'test <- koplsBasicClassify(data = data, k = k)
-#'test
+#' data <- as.matrix(data.frame(x1 = c(2, 1, 5, 1),
+#'                              x2 = c(7, 1, 1, 5),
+#'                              x3 = c(9, 5, 4, 9),
+#'                              x4 = c(3, 4, 1, 2)))
+#' k <- 4
+#' test <- koplsBasicClassify(data = data, k = k)
+#' test
 
 koplsBasicClassify <- function(data, k){
   # Variable format control
@@ -28,7 +38,7 @@ koplsBasicClassify <- function(data, k){
   
   # Search predicted class(es)
   for (i in 1:nrow(data)) {
-    tmp <- which(data[i, ] > k)
+    tmp <- base::which(data[i, ] > k)
     predClass[i, 1:length(tmp)] <- tmp
   }
   
