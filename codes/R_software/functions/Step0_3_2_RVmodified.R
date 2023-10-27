bfc08dc89a5f4edab994d0efb11d5cf5b027a8c7
Celine Bougel <celine.bougel5@laposte.net>
2023-10-27 08:40:32 +0200

    Correction RV formula, Code standardization (packages, function arguments, etc.), Add simulated example for testing.

--- codes/R_software/functions/Step0_3_2_RVmodified.R
+++ codes/R_software/functions/Step0_3_2_RVmodified.R
@@ -5,14 +5,17 @@
 #' calculation is modified for the present method, as it is adjusted over the 
 #' range [0, 1].
 #'
-#' @param X: the normalized meta-kernel of all data block. 
-#' @param Y: the centered and/or scaled kernel of each data block.
+#' @param X: matrix. The normalized meta-kernel of all data block. 
+#' @param Y: matrix. The centered and/or scaled kernel of each data block.
 #'
 #' @return
-#' `RV`: the modified R-square value
+#' `RV`: numeric. The modified R-square value.
 #'
 #' @examples
-#' TO DO
+#' X <- base::matrix(c(1, 2, 3, 4, 5, 6), nrow = 2)
+#' Y <- base::matrix(c(2, 4, 6, 8, 10, 12), nrow = 2)
+#' result <- RVmodified(X = X, Y = Y)
+#' cat("R-Square Value:", result, "\n")
 
 RVmodified <- function(X, Y){
   # Variable format control
@@ -20,14 +23,16 @@ RVmodified <- function(X, Y){
   if(!is.matrix(Y)){stop("Y is not a matrix.")}
   
   # Dimension reduction
-  AA <- X%*%t(X)
-  BB <- Y%*%t(Y)
+  AA <- X %*% t(X)
+  BB <- Y %*% t(Y)
   
   # Similarity matrix
-  AA0 <- AA - diag(diag(AA), nrow(AA), ncol(AA))
-  BB0 <- BB - diag(diag(BB), nrow(BB), ncol(BB))
+  AA0 <- AA - base::diag( base::diag(AA), nrow(AA), ncol(AA))
+  BB0 <- BB - base::diag( base::diag(BB), nrow(BB), ncol(BB))
   
   # Return the R-square value
-  RV <- sum(AA0 %*% BB0) / (sqrt(sum(AA0**2)) * sqrt(sum(BB0**2)))
+  RV <- base::sum( base::diag(AA0 %*% BB0)) / 
+    (base::sqrt( base::sum(AA0**2))) / 
+    (base::sqrt( base::sum(BB0**2)))
   return(RV)
 }
