848eb93f45ef60fc687308cbc25c6fa8eaaee848
Celine Bougel <celine.bougel5@laposte.net>
2023-10-31 15:55:29 +0100

    Code testing and validation with demo data set.

--- codes/R_software/functions/Step0_3_3_DQ2.R
+++ codes/R_software/functions/Step0_3_3_DQ2.R
@@ -8,11 +8,15 @@
 #' @param Y: a matrix containing class labels (0 and 1).
 #'
 #' @return
-#' `dqq`: discriminant Q2 value.
-#' `PRESSD`: the total squared error for discrimination analysis.
+#' `dqq`: numeric. Discriminant Q2 value.
+#' `PRESSD`: matrix. The total squared error for discrimination analysis.
 #'
 #' @examples
-#' TO DO
+#' Y <- base::sample(0:1, 100, replace = TRUE)
+#' Ypred <- stats::runif(100)  
+#' result <- DQ2(Ypred, Y)
+#' base::cat("DQ2:", result$dqq, "\n")
+#' base::cat("PRESSD:", result$PRESSD, "\n")
 
 DQ2 <- function(Ypred, Y){
   # Variable format control
@@ -20,33 +24,34 @@ DQ2 <- function(Ypred, Y){
   if(!is.matrix(Y)){
     stop("Y is not  matrix.")
   } else{
-    if(!(all(Y) %in% c(0,1))){
+    test <- Y %in% c(0, 1)
+    if(all(test) != TRUE){
       stop("Y must contain only 0 or 1 values.")
     }
   }
   
   # Find indices belonging to each Class
-  Class0 <- which(Y == 0)
-  Class1 <- which(Y == 1)
+  Class0 <- base::which(Y == 0)
+  Class1 <- base::which(Y == 1)
   
   # Calculate residuals for each Class
   E0 <- Ypred[Class0] - Y[Class0]   
   E1 <- Ypred[Class1] - Y[Class1]   
   
   # Find predictions for each Class
-  E0count <- which(E0 > 0) # larger than 0
-  E1count <- which(E1 < 0) # smaller than 0
+  E0count <- base::which(E0 > 0) # larger than 0
+  E1count <- base::which(E1 < 0) # smaller than 0
   
   # Calculate SSE for each Class
-  SSE0 <- sum(E0[E0count]**2)
-  SSE1 <- sum(E1[E1count]**2)
+  SSE0 <- base::sum(E0[E0count]**2)
+  SSE1 <- base::sum(E1[E1count]**2)
   
   # Calculate total SSE (PRESSD)
   PRESSD <- SSE0 + SSE1             
   
   # Calculate Total Sum of Squares (TSS)
-  Ym <- Y - mean(Y)                 
-  TSS <- sum(Ym**2)
+  Ym <- Y - base::mean(Y)                 
+  TSS <- base::sum(Ym**2)
   
   # Return DQ2
   dqq <- 1 - (PRESSD / TSS)
