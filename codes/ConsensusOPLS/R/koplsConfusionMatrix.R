fc4e79075037cd180fb1deb26f6c0d31577f3fc9
Thuong Tran <thuong.tran@sib.swiss>
2023-12-21 10:59:35 +0100

    fix koplsConfusionMatrix.R

--- codes/ConsensusOPLS/R/koplsConfusionMatrix.R
+++ codes/ConsensusOPLS/R/koplsConfusionMatrix.R
@@ -8,59 +8,10 @@
 #'
 #' @examples
 #' true_class <- c(1, 2, 1, 2, 3, 3)
-#' pred <- matrix(data = c(1, 2, 3, 2, 3, 3), nrow = length(true_class), ncol = 1)
-#' test <- ConsensusOPLS:::koplsConfusionMatrix(true_class = true_class, 
-#'                                              pred = pred)
-#' test
-
-
-koplsConfusionMatrix <- function(true_class, pred){
-    # Variable format control
-    if (!is.numeric(true_class)) stop("true_class is not numeric.")
-    if (!is.matrix(pred)) stop("pred is not a matrix.")
-    
-    # Extract all classes
-    uniqueClass <- unique(x = true_class)
-    
-    # Check uniqueClass format
-    if (!is.numeric(uniqueClass)){
-        a1 <- koplsDummy(X = true_class, numClasses = NA)
-        colnames(a1) <- 1:nrow(a1)
-        
-        true2 <- matrix(data = NA, nrow = nrow(a1), ncol = 1)
-        true_class <- apply(X = a1, MARGIN = 2,
-                            FUN = function(X){
-                                true2[X > 0, 1] <- colnames(X)
-                            })
-    }
-    
-    # Initialize parameters
-    A <- matrix(data = 0, nrow = length(uniqueClass),
-                ncol = length(uniqueClass))
-    
-    # For each class, find the true class indices
-    indTrue <- lapply(X = uniqueClass, 
-                      FUN = function(cls) which(true_class == cls))
-    
-    # For each class, find the corresponding prediction
-    predIndex <- match(x = pred, table = uniqueClass)
-    
-    # Calculating the occurrences of each unique class
-    pred_freqs <- sapply(X = indTrue,
-                         FUN = function(indices){
-                             if(length(indices) > 0){
-                                 pred_classes <- predIndex[indices]
-                                 pred_counts <- table(pred_classes)
-                                 pred_counts/ length(indices)
-                             } else{
-                                 rep(x = 0, 
-                                     times = length(uniqueClass))
-                             }
-                         })
-    
-    #bug here..
-    A[] <- t(pred_freqs)
-    
-    # Return the confusion matrix
-    return(A)
+#' pred_class <- c(1, 2, 3, 2, 3, 3)
+#' ConsensusOPLS:::koplsConfusionMatrix(true_class = true_class, 
+#'                                              pred_class = pred_class)
+#' @keywords internal
+koplsConfusionMatrix <- function(true_class, pred_class) {
+    return (table(true_class, pred_class))
 }
