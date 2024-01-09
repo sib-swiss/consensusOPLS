51a140bc1d6490f38480bd54d999b5355f913299
Thuong Tran <thuong.tran@sib.swiss>
2024-01-09 15:48:10 +0100

    test demo_3_Omics passed

--- codes/ConsensusOPLS/tests/testthat/test-demo_3_Omics.R
+++ codes/ConsensusOPLS/tests/testthat/test-demo_3_Omics.R
@@ -1,5 +1,5 @@
 test_that("demo_3_Omics", {
-    expect_equal(demo_3_Omics[[1]][1,2], c(MT15709=1.78947304144416))
-    expect_equal(demo_3_Omics[[2]][2,4], c(GC26858=0.433230880710958))
-    expect_equal(demo_3_Omics[[3]][3,6], c(MT1130=1.00069814258038))
+    expect_equal(demo_3_Omics[[1]][1,2], c(MT15709=1.243))
+    expect_equal(demo_3_Omics[[2]][2,4], c(GC26858=219.365316))
+    expect_equal(demo_3_Omics[[3]][3,5], c(MT113=4.739317))
 })
--- codes/ConsensusOPLS/tests/testthat/test-koplsKernel.R
+++ codes/ConsensusOPLS/tests/testthat/test-koplsKernel.R
@@ -1,6 +1,16 @@
-test_that("koplsModel", {
+test_that("koplsKernel", {
     kernel <- koplsKernel(X1 = demo_3_Omics[[1]], X2 = NULL, Ktype = 'p', params = c(order=1))
-    expect_equal(kernel[1,2], -44.6770)
-    expect_equal(kernel[6,3], 0.8963)
-    expect_equal(kernel[9,11], -13.2810)
+    expect_equal(kernel[1,2], 175.6044, tolerance=1e-4)
+    expect_equal(kernel[6,3], 228.6084, tolerance=1e-4)
+    expect_equal(kernel[9,11], 145.23854, tolerance=1e-4)
+    
+    kernel <- koplsKernel(X1 = demo_3_Omics[[2]], X2 = NULL, Ktype = 'p', params = c(order=0.5))
+    expect_equal(kernel[1,2], 9085.007, tolerance=1e-4)
+    expect_equal(kernel[6,3], 11383.501, tolerance=1e-4)
+    expect_equal(kernel[9,11], 9438.85, tolerance=1e-4)
+    
+    kernel <- koplsKernel(X1 = demo_3_Omics[[3]], X2 = NULL, Ktype = 'p', params = c(order=0.1))
+    expect_equal(kernel[1,2], 8.560449, tolerance=1e-5)
+    expect_equal(kernel[6,3], 9.715536, tolerance=1e-5)
+    expect_equal(kernel[9,11], 9.892736, tolerance=1e-5)
 })
